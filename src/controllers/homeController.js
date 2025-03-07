bf98ed64ff022af686259e7eee5fbc1ca9b134ea
Thanh Long <longvietnam1182001@gmail.com>
2025-03-07 10:06:05 +0700

    create DB

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,15 @@
+import db from '../models/index';
+let getHomePage = async(req,res) => {
 
-let getHomePage = (req,res) => {
+    try{
+        let data = await db.User.findAll();
+
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        }); 
+    }catch(e){
+        console.log(e) 
+    }
     return res.render('homepage.ejs'); 
 }
 
